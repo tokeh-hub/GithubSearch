dc266fdbc61311c7c96128d59ffa153ef6e5c4a2
tokeh-hub <olatunjiaisha12@gmail.com>
2022-08-21 22:16:12 +0100

    renewed expired token

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient,HttpHeaders} from "@angular/common/http"
-import { Observable } from 'rxjs';
-import { User } from 'user';
+
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/vnd.github+json',
-    'Authorization': `Bearer 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`
+    'Authorization': `Bearer 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`
   })
 }
 
@@ -16,11 +15,6 @@ export class SearchService {
   private apiUrl = `https://api.github.com/search/users`
  
   constructor(private http:HttpClient) { }
-  
-  // searchUser(text: string): Observable<string>{
-  //   const url = `${this.apiUrl}?q=${text}`
-  //   return this.http.get<string>(url,httpOptions)
-  // }
 
   getUsers(text:any,page:number,perPage:number){
     const url = `${this.apiUrl}?q=${text}&page=${page}&per_page=${perPage}`
